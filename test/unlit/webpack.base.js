033a1b181512e68bb4cf307c0374ada79f049cd9
guoaihua <guoaihua@geetest.com>
2020-04-21 10:49:31 +0800

    chore: update travis.yml

--- test/unlit/webpack.base.js
+++ test/unlit/webpack.base.js
@@ -2,7 +2,7 @@ const assert = require('assert');
 describe('webpack test case', function () {
     const baseconfig = require("../../lib/webpack.base");
     it('entry', function () {
-        assert.equal(baseconfig.entry.index, '/Users/ziming/Tools/webpackbuilder/test/smoke/template/src/index/index.js');
-        assert.equal(baseconfig.entry.test,"/Users/ziming/Tools/webpackbuilder/test/smoke/template/src/test/index.js")
+        assert.equal(baseconfig.entry.index.indexOf('webpackbuilder/test/smoke/template/src/index/index.js') >-1 ,true);
+        assert.equal(baseconfig.entry.test.indexOf("webpackbuilder/test/smoke/template/src/test/index.js")>-1,true)
     });
 });
