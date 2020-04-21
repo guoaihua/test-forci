33daf0bc0dd30c55d4192c10a60622e3df65f300
guoaihua <guoaihua@geetest.com>
2020-04-21 10:54:16 +0800

    chore: update config

--- test/unlit/webpack.base.js
+++ test/unlit/webpack.base.js
@@ -2,7 +2,7 @@ const assert = require('assert');
 describe('webpack test case', function () {
     const baseconfig = require("../../lib/webpack.base");
     it('entry', function () {
-        assert.equal(baseconfig.entry.index.indexOf('webpackbuilder/test/smoke/template/src/index/index.js') >-1 ,true);
-        assert.equal(baseconfig.entry.test.indexOf("webpackbuilder/test/smoke/template/src/test/index.js")>-1,true)
+        assert.equal(baseconfig.entry.index.indexOf('/test/smoke/template/src/index/index.js') >-1 ,true);
+        assert.equal(baseconfig.entry.test.indexOf("/test/smoke/template/src/test/index.js")>-1,true)
     });
 });
